07c99c68eb519c028f71b0115d1ff1456c96c165
Kim Hyun Jin <wlalsl4267@gmail.com>
2025-01-03 17:01:34 +0900

    fix

--- Sources/LookheartPackage/Summary/LineChart/Services/LineChartController.swift
+++ Sources/LookheartPackage/Summary/LineChart/Services/LineChartController.swift
@@ -163,8 +163,7 @@ class LineChartController {
         lineChartModel: LineChartModel
     ) -> Bool {
         // 1. entries
-        print("entries: \(lineChartModel.entries?.count)")
-        guard let entries = lineChartModel.entries, entries.count <= 0 else {
+        guard let entries = lineChartModel.entries, entries.count > 0 else {
             return false // noData
         }
         
